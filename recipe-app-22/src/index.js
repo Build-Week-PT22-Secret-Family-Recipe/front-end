b5eceeec8b5f24772c809ecb109e3e9ea040504f
chqiu6 <chucky-qiu@lambdastudents.com>
2021-01-27 20:02:53 -0500

    Basic Sign up form, missing axios request on formSubmit

--- recipe-app-22/src/index.js
+++ recipe-app-22/src/index.js
@@ -1,5 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './Components/App'
+
 
 ReactDOM.render(<App />, document.querySelector('#root'));
